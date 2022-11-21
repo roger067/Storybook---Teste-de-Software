1ca194fb99ef21a51aa2424f4affd68b92f57cfe
roger067 <rosestolato.rogerio@gmail.com>
2022-11-21 18:13:45 -0300

    adding button test

---
+++ src/Button/Button.spec.tsx
@@ -0,0 +1,59 @@
+import { screen, render, fireEvent } from "@testing-library/react";
+
+import { COLORS } from "../utils";
+import Button from "./Button";
+
+test("should render button component", () => {
+  render(<Button variant="primary">Button</Button>);
+
+  const button = screen.getByRole("button");
+
+  expect(button).toBeInTheDocument();
+});
+
+test("should render correctly text", () => {
+  render(<Button variant="primary">Texto</Button>);
+
+  const button = screen.getByText("Texto");
+
+  expect(button).toBeInTheDocument();
+});
+
+test("should render primary button", () => {
+  render(<Button variant="primary">Texto</Button>);
+
+  const button = screen.getByRole("button");
+
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveStyle(`background-color: ${COLORS.GREEN_600}`);
+  expect(button).toHaveStyle(`color: ${COLORS.WHITE}`);
+});
+
+test("should render secondary button", () => {
+  render(<Button variant="secondary">Texto</Button>);
+
+  const button = screen.getByRole("button");
+
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveStyle(`background-color: ${COLORS.WHITE}`);
+  expect(button).toHaveStyle(`color: ${COLORS.GREEN_600}`);
+});
+
+test("should render disabled button", () => {
+  const mockedOnClick = jest.fn();
+
+  render(
+    <Button variant="secondary" disabled onClick={mockedOnClick}>
+      Texto
+    </Button>
+  );
+
+  const button = screen.getByRole("button");
+
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveStyle(`background-color: ${COLORS.GREY_300}`);
+  expect(button).toHaveStyle(`color: ${COLORS.GREY_600}`);
+
+  fireEvent.click(button);
+  expect(mockedOnClick).toHaveBeenCalledTimes(0);
+});
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,51 +1,53 @@
-import React from "react";
+import styled, { css } from "styled-components";
+import { COLORS } from "../utils";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  variant?: "primary" | "secondary";
+  disabled?: boolean;
+  rounded?: boolean;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
-  return (
-    <button
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+const primaryCss = css`
+  background-color: ${COLORS.GREEN_600};
+  color: ${COLORS.WHITE};
+  border: none;
+
+  &:hover {
+    background-color: ${COLORS.GREEN_700};
+  }
+`;
+
+const secondaryCss = css`
+  background-color: ${COLORS.WHITE};
+  color: ${COLORS.GREEN_600};
+  border: 1px solid ${COLORS.GREEN_600};
+
+  &:hover {
+    background-color: ${COLORS.GREEN_700};
+    color: ${COLORS.WHITE};
+    border: 1px solid ${COLORS.GREEN_700};
+  }
+`;
+
+const buttonStyle = {
+  primary: primaryCss,
+  secondary: secondaryCss,
 };
+
+const Button = styled.button<ButtonProps>`
+  ${({ variant }) => buttonStyle[variant || "primary"]};
+  border-radius: ${({ rounded }) => (rounded ? "16px" : "4px")};
+  font-size: 14px;
+  padding: 8px 16px;
+  cursor: pointer;
+  transition: all 250ms;
+
+  &:disabled {
+    background-color: ${COLORS.GREY_300};
+    color: ${COLORS.GREY_600};
+    cursor: not-allowed;
+    border: none;
+  }
+`;
+
+export default Button;
